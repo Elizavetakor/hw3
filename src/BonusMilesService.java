c6749c4ad521c7c87dc91da1d8ced472ec24aa59
Elizaveta <aliza-97@mail.ru>
2023-10-31 21:25:40 +0300

    beautiful code

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,7 +1,7 @@
 public class BonusMilesService {
-    public int calculate(int prise){
-int b = 20;
-int miles = prise / b;
-return miles;
+    public int calculate(int prise) {
+        int b = 20;
+        int miles = prise / b;
+        return miles;
     }
 }
